a8d289c7c9acf6926319d208e8a9495683bd8bd2
razor389 <rgranowski@gmail.com>
2024-09-18 12:54:56 -0500

    empty file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+// main.rs
+mod scanner;
+mod token;
+
 use std::env;
 use std::fs;
 use std::io::{self, Write};
+use scanner::Scanner;
+use token::TokenType;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,24 +20,28 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
-            //Uncomment this block to pass the first stage
             if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
+                let mut scanner = Scanner::new(file_contents);
+                scanner.scan_tokens();
+
+                for token in scanner.get_tokens() {
+                    match token.token_type {
+                        TokenType::LEFT_PAREN => println!("LEFT_PAREN ( null"),
+                        TokenType::RIGHT_PAREN => println!("RIGHT_PAREN ) null"),
+                        TokenType::EOF => println!("EOF  null"),
+                    }
+                }
             } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+                println!("EOF  null");
             }
         }
         _ => {
             writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
         }
     }
 }
